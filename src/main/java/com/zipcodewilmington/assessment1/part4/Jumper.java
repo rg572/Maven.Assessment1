6b1409ad09ce34b32b03745181cfd441a63abf27
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-03 13:03:14 -0400

    jumper done

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,11 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+        if(j <= 1){
+            return k;
+        }
+        else{
+            return k/j + k%j;
+        }
     }
 }
